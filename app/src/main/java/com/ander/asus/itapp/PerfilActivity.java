3389cad7d9d0c06c4fdb853176278b18029ababe
HumbertoTapia <humbertotapia360@gmail.com>
2020-09-02 18:55:13 -0500

    Implementacion del modulo de manual

--- app/src/main/java/com/ander/asus/itapp/PerfilActivity.java
+++ app/src/main/java/com/ander/asus/itapp/PerfilActivity.java
@@ -2,7 +2,9 @@ package com.ander.asus.itapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PerfilActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class PerfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
     }
+
+
+    //metodo para llamar al activity Estudiante
+    public void llamarEstudiate(View view) {
+        Intent i = new Intent(this, EstudianteActivity.class );
+        startActivity(i);
+    }
+
 }
